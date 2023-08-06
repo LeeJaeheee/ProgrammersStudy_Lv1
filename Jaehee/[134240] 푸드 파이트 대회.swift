914f415a576cb8d7f80a4dd58383ebd461cca231
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-08-07 08:48:57 +0900

    [134240] 푸드 파이트 대회

--- Jaehee/[134240] 푸드 파이트 대회.swift
+++ Jaehee/[134240] 푸드 파이트 대회.swift
@@ -1,1 +1,11 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/134240
+
+import Foundation
+
+func solution(_ food:[Int]) -> String {
+    var result = ""
+    for (i, f) in food.enumerated() {
+        result += String(repeating: String(i), count: f/2)
+    }
+    return result + "0" + result.reversed()
+}
